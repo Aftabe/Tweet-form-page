1ce6569eccdbbb10e6bbea5d73c57b4c146f287c
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-01-30 20:28:11 +1100

    new js statements added

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-var doSomething = function(paramOne, paramTwo, fn) {
-    paramOne = paramOne + 3;
-    paramOne = paramOne + 1;
-    paramOne = paramOne * 8;
+// var doSomething = function(paramOne, paramTwo, fn) {
+//     paramOne = paramOne + 3;
+//     paramOne = paramOne + 1;
+//     paramOne = paramOne * 8;
 
-    return fn(paramOne, paramTwo);
-};
+//     return fn(paramOne, paramTwo);
+// };
 
-function sum(paramOne, paramTwo) {
-    return paramOne + paramTwo;
-}
+// function sum(paramOne, paramTwo) {
+//     return paramOne + paramTwo;
+// }
 
 
 
-// set one
-var foo = doSomething(2, 2, sum); // 96
+// // set one
+// var foo = doSomething(2, 2, sum); // 96
 
-// set two
-var bar = doSomething(3, 2, function(paramOne, paramTwo) {
-    return paramOne * paramTwo;    
-}); // 112
+// // set two
+// var bar = doSomething(3, 2, function(paramOne, paramTwo) {
+//     return paramOne * paramTwo;    
+// }); // 112
 
-alert(foo);
-alert(bar);
+// alert(foo);
+// alert(bar);
+
+var too = 2 + 3,
+    too = too + 1,
+    too = too * 5;
+
+
+var soo = 3 + 3,
+    soo = soo + 1,
+    soo = soo * 5;
+    
+
+alert(too);    
+alert(soo);    
