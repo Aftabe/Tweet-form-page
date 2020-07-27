d9b19fff71fd0cf5c5aad7e8931abfe7323804c0
Sunny <47844914+Aftabe@users.noreply.github.com>
2020-07-27 19:21:09 +1000

    new js code block added

--- script.js
+++ script.js
@@ -148,3 +148,18 @@ function localStorageLoad() {
   });
 
 }
+
+
+// The js switch statements
+var text;
+switch (new Date().getDay()) {
+  default:
+    text = "Looking forward to the Weekend";
+    break;
+  case 6:
+    text = "Today is Saturday";
+    break;
+  case 0:
+    text = "Today is Sunday";
+}
+document.getElementById("demo").innerHTML = text;
